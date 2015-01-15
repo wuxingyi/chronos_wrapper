f9c5d0963b8386ce680db8433a0b6a213f47d114
wuxingyi <wuxingyi@letv.com>
2015-01-15 15:29:42 +0800

    check whether the job is exist before check the job is successful

--- deleteAllJobs.sh
+++ deleteAllJobs.sh
@@ -1,5 +1,5 @@
 
-curl -X GET -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -H "Content-Type: application/json" http://10.180.92.57:9999/scheduler/jobs > test.json
+curl -X GET -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -H "Content-Type: application/json" http://10.180.92.$1:9999/scheduler/jobs > test.json
 
 #./jobinfo.py test.json $1 | python -m json.tool
 ./helperGetJobNames.py test.json
@@ -7,7 +7,7 @@ curl -X GET -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -H "Content-Type:
 
 while read LINE
 do
-   curl -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -L -X DELETE http://10.180.92.57:9999/scheduler/job/$LINE
+   curl -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -L -X DELETE http://10.180.92.$1:9999/scheduler/job/$LINE
 done < Names.txt
 #cat hahaha.json | python -m json.tool
 rm test.json Names.txt -rf
--- isSuccessfull.sh
+++ isSuccessfull.sh
@@ -1,3 +1,20 @@
+./getJobNames.sh > result.txt
+
+hasJob=0
+while read line
+do
+    if [ "$line" = $1 ];then
+       hasJob=1
+       break
+    fi
+done < result.txt
+
+if [ $hasJob -eq 0  ];then
+    echo "no such job"
+    exit -1
+fi
+
+
 ./getJobDetails.sh $1 > result.txt
 str=`cat result.txt | grep "lastSuccess" | awk -F \": '{print $2}'`
 if [ "$str" = ' "", ' ];then
