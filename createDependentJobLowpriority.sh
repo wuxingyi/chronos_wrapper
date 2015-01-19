7070cfd67f1a6d1926c16024e60d27e3d92a8c1a
wuxingyi <wuxingyi@letv.com>
2015-01-19 16:03:45 +0800

    add stop task API

--- createDependentJobLowpriority.sh
+++ createDependentJobLowpriority.sh
@@ -1,4 +1,4 @@
-str="{\"name\": "\"$1\"", \"command\": \"sleep 10\", \"epsilon\": \"PT30S\", \"owner\": \"wuxingyi@letv.com\", \"cpus\": 0.1, \"disk\": 0, \"mem\": 16, \"highPriority\": false, \"runAsUser\": \"root\", \"parents\": [\"1\"]}" 
+str="{\"name\": "\"$1\"", \"command\": \"sleep 10\", \"epsilon\": \"PT30S\", \"owner\": \"wuxingyi@letv.com\", \"cpus\": 0.1, \"disk\": 0, \"mem\": 16, \"highPriority\": false, \"runAsUser\": \"root\", \"parents\": [\"$2\"]}" 
 
 
 curl -X POST -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -H "Content-Type: application/json" -d "${str}" http://10.180.92.57:9999/scheduler/dependency
---
+++ longrunjob.sh
@@ -0,0 +1,6 @@
+str="{\"name\": "\"$1\"", \"command\": \"sleep 100000\", \"epsilon\": \"PT30S\", \"owner\": \"wuxingyi@letv.com\", \"async\": false, \"cpus\": 0.1, \"disk\": 0, \"mem\": 16, \"highPriority\": true, \"runAsUser\": \"root\", \"schedule\": \"R10/2015-01-19T$3:$4:00+08:00/PT1M\"}" 
+
+
+curl -X POST -H "Authorization: Basic cm9vdDpoZWxsb3dvcmxkMTIz" -H "Content-Type: application/json" -d "${str}" http://10.180.92.$2:9999/scheduler/iso8601
+
+
